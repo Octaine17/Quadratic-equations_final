185fa3407521e1f666ee036a4e3fca17d0c1e89f
Octaine17 <49759185+Octaine17@users.noreply.github.com>
2021-12-08 09:35:58 +0700

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,6 +15,10 @@ public class Main {
         NoException, Normal, FullException
     }
 
+    enum ProcessType {
+        Sequence, Paraller
+    }
+
     public static class ThrowableVector extends Throwable {
         Vector<Double> vector = new Vector<Double>();
     }
@@ -52,13 +56,11 @@ public class Main {
     }
 
 
-    static Object solve_no_exceptions(double a, double b, double c, boolean ok) {
-        ok = true;
+    static Pair<Boolean, Vector<Double>> solve_no_exceptions(double a, double b, double c) {
         if (isEqual(a, 0) && isEqual(b, 0) && isEqual(c, 0)) {
-            ok = false;
-            return new Pair<>(ok,new Vector<Double>(0));
+            return new Pair<>(false, new Vector<Double>());
         }
-        return new Pair<>(ok, solve_correct_equation(a, b, c));
+        return new Pair<>(true, solve_correct_equation(a, b, c));
     }
 
     static double call_solver(FuncType type, double a, double b, double c) {
@@ -91,7 +93,7 @@ public class Main {
     }
 
     static double roots_sum_no_exception(double a, double b, double c) {
-        Pair pair = (Pair) solve_no_exceptions(a, b, c, true);
+        Pair pair = (Pair) solve_no_exceptions(a, b, c);
         if (true == (boolean) pair.getKey()) {
             return sum_vec((Vector<Double>) pair.getValue());
         }
@@ -136,55 +138,67 @@ public class Main {
 
 
     public static void main(String[] args) {
-        final int from = 1024;
-        final int to = from * 512;
+        final int from = 32768;
+        final int to = 2097152;
         System.out.println("\t\t\t--------Normal-----------");
         for (long i = from; i <= to; i *= 2) {
-            run(i, FuncType.Normal);
+            run(i, FuncType.Normal, ProcessType.Sequence);
         }
         System.out.println("\t\t\t--------No exception-----------");
         for (long i = from; i <= to; i *= 2) {
-            run(i, FuncType.NoException);
+            run(i, FuncType.NoException, ProcessType.Sequence);
         }
         System.out.println("\t\t\t--------FullException-----------");
         for (long i = from; i <= to; i *= 2) {
-            run(i, FuncType.FullException);
+            run(i, FuncType.FullException, ProcessType.Sequence);
         }
     }
 
-    static void run(long n, FuncType type) {
+    static void run(long n, FuncType type, ProcessType processType) {
         long time = System.nanoTime();
+        if (processType == ProcessType.Sequence) {
+            run_sequence(0, n, type);
+        } else if (processType == ProcessType.Paraller) {
+            run_parallel(n, type);
+        }
+        time = System.nanoTime() - time;
+        System.out.printf("Elapsed Consecutively %,9.3f ms\n", time / 1_000_000.0);
+    }
+
+    public static void run_parallel(long n, FuncType type) {
+        AtomicReference<Double> global_sum = new AtomicReference<>((double) 0);
+
+        final int thread_num = Runtime.getRuntime().availableProcessors();
+        ExecutorService executor = Executors.newFixedThreadPool(thread_num);
+        Lock lock = new ReentrantLock();
+        long data_per_thread = n / thread_num;
+        for (int thread_id = 0; thread_id < thread_num; ++thread_id) {
+            final int thread_id_copy = thread_id;
+            executor.submit(() -> {
+                long start = thread_id_copy * data_per_thread;
+                long end = start + data_per_thread;
+                if (thread_id_copy == thread_num - 1) {
+                    end = n;
+                }
+
+                double local_sum = run_sequence(start,end,type);
+                synchronized (global_sum) {
+                    double local_sum_copy = local_sum;
+                    global_sum.updateAndGet(v -> new Double((double) (v + local_sum_copy)));
+                }
+            });
+        }
+        executor.shutdown();
+    }
+
+    public static Double run_sequence(long from, long to, FuncType type) {
         double sum = 0;
-        for (long i = 0; i < n; i++) {
+        for (long i = from; i < to; i++) {
             double a = ((i % 2000) - 1000) / 33.0;
             double b = ((i % 200) - 100) / 22.0;
             double c = ((i % 20) - 10) / 11.0;
             sum += call_solver(type, a, b, c);
         }
-        time = System.nanoTime() - time;
-        System.out.printf("Elapsed Consecutively %,9.3f ms\n", time / 1_000_000.0);
-        time = System.nanoTime();
-        AtomicReference<Double> final_sum = new AtomicReference<>((double) 0);
-        ExecutorService executor = Executors.newFixedThreadPool(4);
-        Lock lock = new ReentrantLock();
-        executor.submit(() -> {
-            double temp_sum = 0;
-            for (long i = 0; i < n; i++) {
-                double a = ((i % 2000) - 1000) / 33.0;
-                double b = ((i % 200) - 100) / 22.0;
-                double c = ((i % 20) - 10) / 11.0;
-                temp_sum += call_solver(type, a, b, c);
-            }
-            lock.lock();
-            try {
-                double finalTemp_sum = temp_sum;
-                final_sum.updateAndGet(v -> new Double((double) (v + finalTemp_sum)));
-            } finally {
-                lock.unlock();
-            }
-            executor.shutdown();
-        });
-        time = System.nanoTime() - time;
-        System.out.printf("Elapsed Parrarel      %,9.3f ms\n", time / 1_000_000.0);
+        return sum;
     }
 }
